c8aaf14043517dc3ff62e036c068b19c5cba7c69
XxXMatheusXxX <matheus.physicist369@gmail.com>
2024-03-14 14:14:06 -0300

    kk

---
+++ src/main/java/com/testo/AtividadeApplication.java
@@ -0,0 +1,13 @@
+package com.testo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeApplication.class, args);
+	}
+
+}
